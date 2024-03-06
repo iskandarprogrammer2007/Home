f1e667badebe017025c0ec5dc6bb3a8cb0048840
iskandarprogrammer2007 <abdumalikoviskandar600@gmail.com>
2024-03-06 18:30:03 +0500

    adedd all

--- js/main.js
+++ js/main.js
@@ -1,28 +1,17 @@
 let backTop = document.querySelector(".back-top");
 let header = document.querySelector(".header");
 let body = document.querySelector("body");
-let darkMode = document.querySelector(".header__nav__right__btns__dark-mode");
 let headerList = document.querySelector(".header__nav__right__list");
 let hamburger = document.querySelector(".header__nav__right__btns__hamburger");
 let closeHeaderList = document.querySelector(".header__nav__right__list__close");
-let dropdown = document.querySelector(".dropdown")
-let dropdownList = document.querySelector(".dropdown__list")
-let dropdownIcon = document.querySelector(".dropdown__icon")
 
 
-dropdown.addEventListener("click",function(){
-  dropdownList.classList.toggle("show__dropdown__list");
-  dropdownIcon.classList.toggle("show__dropdown-icon");
-})
 
 window.addEventListener("scroll", function () {
   showBackTop();
   showHeaderShrink();
 });
 
-darkMode.addEventListener("click", function () {
-  body.classList.toggle("dark");
-});
 
 hamburger.addEventListener("click", function () {
   headerList.classList.toggle("show__header__nav__right__list");
